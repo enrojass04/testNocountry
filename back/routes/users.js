1aee9b8460d18c89654b06f09c50ece2203da2a8
Edwin <enrojass04@example.com>
2024-06-10 07:46:06 -0500

    ss

--- back/routes/users.js
+++ back/routes/users.js
@@ -8,7 +8,7 @@ import {
 
 const router = express.Router();
 
-router.get("/", checkAuthorization, getAllUsers);
+router.get("/", getAllUsers);
 router.get("/page", checkAuthorization, getAllUsersPaginated);
 router.get("/:id", checkAuthorization, retrieveUserById);
 
